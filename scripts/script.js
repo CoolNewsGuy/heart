07d38e3f70e1be9db2fc321d2c5554e1c969c0e4
MrLuckyCat <joewilliamdd31@gmail.com>
2022-12-28 11:54:31 +0100

    Create Heart object using the module pattern

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+const Heart = (function () {
+   let heart = document.querySelector(".heart"),
+      parts = document.querySelectorAll(".half"),
+      name = document.querySelector(".name");
+
+   return {
+      shape,
+      parts,
+      name,
+   };
+})();
