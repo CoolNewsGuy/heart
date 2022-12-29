2b83a083d1af0df8f64bb5836d2bd865bc3da840
MrLuckyCat <joewilliamdd31@gmail.com>
2022-12-29 15:39:15 +0100

    Add ShadowHeart object

---
+++ scripts/shadow-heart.js
@@ -0,0 +1,9 @@
+const ShadowHeart = (function () {
+   let heart = document.querySelector(".heart-shadow"),
+      parts = document.querySelectorAll(".shadow-half");
+
+   return {
+      heart,
+      parts,
+   };
+})();
