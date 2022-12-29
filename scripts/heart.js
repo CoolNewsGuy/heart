382272a48d347163e9d88518114a05e9dcb30a48
MrLuckyCat <joewilliamdd31@gmail.com>
2022-12-29 22:47:58 +0100

    Recreate shadow-heart

--- scripts/heart.js
+++ scripts/heart.js
@@ -10,24 +10,16 @@ const Heart = (function () {
       name,
       beat,
       color: "red",
-      changeHeartColor,
    };
 
    function beat() {
-      heart.classList.add("beated-heart");
+      Heart.heart.classList.add("beated-heart");
 
-      setTimeout(() => heart.classList.remove("beated-heart"), 400);
+      setTimeout(() => {
+         Heart.heart.classList.remove("beated-heart");
+      }, 400);
       setTimeout(beat, 1200);
    }
-
-   function changeHeartColor() {
-      parts.forEach((part) => (part.style.background = ShadowHeart.color));
-
-      ShadowHeart.parts.forEach(
-         (part) => (part.style.background = Heart.color)
-      );
-
-      // switch the colors
-      [Heart.color, ShadowHeart.color] = [ShadowHeart.color, Heart.color];
-   }
 })();
+
+// onload = Heart.beat;
