94d2228ec9b306602fdf955a0444d4f4219f225f
MrLuckyCat <joewilliamdd31@gmail.com>
2022-12-30 10:05:34 +0100

    Add a function and animation to remove the stars

--- scripts/stars.js
+++ scripts/stars.js
@@ -1,20 +1,27 @@
 const createStar = function () {
-   let star = document.querySelector(".star").cloneNode();
+   let star = document.querySelector(".star").cloneNode(),
+      hideAnimationTime = Math.random() * 12 + 6;
 
    star.style.cssText = `
       position: absolute;
       top: ${Math.random() * 952}px;
       left: ${Math.random() * 1921}px;
       z-index: 999;
+      animation: hide-star ${hideAnimationTime}s linear;
    `;
 
    function animateStar() {
       star.style.top = +star.style.top.replace("px", "") + 1 + "px";
    }
 
+   function removeStar() {
+      document.body.removeChild(star);
+   }
+
    setInterval(animateStar, 100);
+   setTimeout(removeStar, hideAnimationTime * 1000);
 
    document.body.insertBefore(star, document.querySelector(".star"));
 };
 
-// for (let i = 0; i < 50; i++) createStar();
+for (let i = 0; i < 50; i++) createStar();
